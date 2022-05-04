e933efa9aa064278fde99969835c136e7e75a495
Tiago Longueira <tlongueira@cdt.com.ar>
2022-05-04 09:21:11 -0300

    primer commit

---
+++ src/main/java/com/tlongueira/calculadora/CalculadoraCursoApplication.java
@@ -0,0 +1,13 @@
+package com.tlongueira.calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraCursoApplication.class, args);
+	}
+
+}
