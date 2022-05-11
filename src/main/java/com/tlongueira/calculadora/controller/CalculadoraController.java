0f38e63e78e5b2c9b56e8ab369d1061c4a5ba66f
Tiago Longueira <tlongueira@cdt.com.ar>
2022-05-11 09:05:14 -0300

    http update

--- src/main/java/com/tlongueira/calculadora/controller/CalculadoraController.java
+++ src/main/java/com/tlongueira/calculadora/controller/CalculadoraController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,19 +25,19 @@ public class CalculadoraController {
 	public ResponseEntity<String>  estoyVivo(){
 		return new ResponseEntity<String>("Vivo", HttpStatus.OK);
 	}
-	@GetMapping("/sumar")
+	@PostMapping("/sumar")
 	public ResponseEntity<ResultadoOperacion>  sumar(@Valid @RequestBody ValoresDTO valoresDTO){
 		return new ResponseEntity<ResultadoOperacion>(calculadoraService.sumar(valoresDTO), HttpStatus.OK);
 	}
-	@GetMapping("/restar")
+	@PostMapping("/restar")
 	public ResponseEntity<ResultadoOperacion>  restar(@Valid @RequestBody ValoresDTO valoresDTO){
 		return new ResponseEntity<ResultadoOperacion>(calculadoraService.restar(valoresDTO), HttpStatus.OK);
 	}
-	@GetMapping("/multiplicar")
+	@PostMapping("/multiplicar")
 	public ResponseEntity<ResultadoOperacion>  multiplicar(@Valid @RequestBody ValoresDTO valoresDTO){
 		return new ResponseEntity<ResultadoOperacion>(calculadoraService.mutiplicar(valoresDTO), HttpStatus.OK);
 	}
-	@GetMapping("/dividir")
+	@PostMapping("/dividir")
 	public ResponseEntity<ResultadoOperacion>  dividir(@Valid @RequestBody ValoresDTO valoresDTO){
 		return new ResponseEntity<ResultadoOperacion>(calculadoraService.dividir(valoresDTO), HttpStatus.OK);
 	}
